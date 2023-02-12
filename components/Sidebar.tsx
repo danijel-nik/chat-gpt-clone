e2bacb86e534b5cc7c3eac74919d4876c7064a87
danijel_nik <danijel.nikolic@ymail.com>
2023-02-12 01:53:55 +0100

    firebase connection

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,19 +1,31 @@
+'use client'
+import { useSession, signOut } from "next-auth/react"
 import NewChat from "./NewChat"
 
 const Sidebar = () => {
-    return (
-        <div className="p-2 flex flex-col h-screen">
-					<div className="flex-1">
-						<NewChat />
+	const { data: session } = useSession()
+	return (
+		<div className="p-2 flex flex-col h-screen">
+			<div className="flex-1">
+				<NewChat />
 
-						<div>
-							{/* ModelSelection */}
-						</div>
+				<div>
+					{/* ModelSelection */}
+				</div>
 
-						{/* Map through the ChatRows */}
-					</div>
-        </div>
-    )
+				{/* Map through the ChatRows */}
+			</div>
+
+			{session && (
+				<img
+					onClick={() => signOut()}
+					src={session.user?.image || ''}
+					alt="Profile picture"
+					className="w-12 h-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+				/>
+			)}
+		</div>
+	)
 }
 
 export default Sidebar
